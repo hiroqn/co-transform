e0696725c428c599eb3cbdf10767d1b6561aa8e8
hiroqn <hiroqn1008@gmail.com>
2015-12-22 04:12:28 +0900

    feat(index.js): add main file

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+const Transform = require('stream').Transform;
+
+module.exports = function (generator, options) {
+  const stream = new Transform(options);
+  let result;
+  let error = null;
+  const iterator = generator.call(Object.assign(stream, {
+    _transform(chunk, encoding, cb) {
+      if (result.done || error) {
+        return cb(error);
+      }
+
+      try {
+        result = iterator.next(chunk);
+      } catch (e) {
+        error = e;
+      }
+      cb(error);
+    },
+    _flush(cb) {
+      if (result.done || error) {
+        return cb(error);
+      }
+
+      try {
+        result = iterator.next(null);
+      } catch (e) {
+        error = e;
+      }
+      cb();
+    }
+  }));
+  try {
+    result = iterator.next();
+  } catch (e) {
+    error = e;
+  }
+  return stream;
+};
